6a741e7477c9ffa085a8f78a619abf49bd622ed6
rayanlb6 <toukaprayan6@gmail.com>
2026-05-02 07:25:23 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import { useState } from 'react';
 function App() {
-  const [criteres, setCriteres] = useState(['Prix', 'Qualité', 'Design']);
-  const [alts, setAlts] = useState(['Option A', 'Option B']);
+  const [criteres] = useState(['Prix', 'Qualité', 'Design']);
+  const [alts] = useState(['Option A', 'Option B']);
   const [matrice, setMatrice] = useState([[1,3,5],[0.333,1,2],[0.2,0.5,1]]);
   const [res, setRes] = useState(null);
   const calc = () => {
